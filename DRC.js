084d0f7cb552fc0acd863775ea83f29e317e7293
Abhi-1994 <abhitrueblue7@gmail.com>
2018-01-05 14:27:14 -0500

    Add files via upload

---
+++ DRC.js
@@ -0,0 +1,276 @@
+{
+  "items": [
+    {
+      "label": "Haiti Earthquake, Field Investigation on Comparative Performance of Alternative Humanitarian Logistic Structures",
+      "type": "Disaster",
+      "location": "Miami, FL; Homestead, FL; Ft. Lauderdale, FL, U.S.A.",
+      "Month": "January",
+      "Year": "2010",
+      "purpose": " This project researched the hosting of evacuees in the Miami area from the January, 2010 earthquake in Haiti. The field team interviewed public and nonprofit decision makers and analyzed agency reports and news media accounts. In May of 2010 James Kendra and Sudha Arlikatti (University of North Texas) and Manuel Torres (University of Delaware) traveled to the Miami area for interviews and site visits.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Haiti Earthquake, The Forgotten Aspects of Evacuation",
+      "type": "Disaster",
+      "location": "Miami, FL, U.S.A.",
+      "latlng": "27,-82",
+      "Month": "January",
+      "Year": "2010",
+      "purpose": "To collect perishable data on the relief effort underway in Florida to assist earthquake stricken Haiti. Time was spent observing activities at warehouses, donation drives, Homestead Air Reserve Base, and planning meetings. The reception process of incoming evacuees from Haiti was also observed at Miami International Airport. Informal interviews were conducted with participants involved in these activities. Later, research was conducted in Port-au-Prince and Leogane, Haiti to attempt to understand the causes of the initial delay in the international relief as blamed on a shortage of trucks and warehouses, and the organization of this effort from the logistical viewpoint once started. The study consisted of 25 qualitative interviews with key informants from the United Nations agencies, the Red Cross, the representatives of grassroots organizations and the citizenry.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Hurricane Charlie, Post-Hurricane Recovery",
+      "type": "Disaster",
+      "location": "Punta Gorda, FL, U.S.A.",
+      "Month": "January",
+      "Year": "2010",
+      "purpose": "To collect data on recovery of City of Punta Gorda following Hurricane Charlie",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Hurricane"
+    },
+    {
+      "label": "Haiti Earthquake, Material Convergence",
+      "type": "Disaster",
+      "location": "Dominican Republic",
+      "latlng": "19,-70",
+      "Month": "January",
+      "Year": "2010",
+      "purpose": "This research effort was focused on collecting perishable data and field observations related to the response of the Dominican Republic and its coordination efforts to channel aid into neighboring Haiti. In addition, this project sought to generate a better understanding of the relationship between the two countries that comprise the island of Hispaniola, particularly to explore pre- and post-disaster migration from Haiti to the Dominican Republic. ",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Hurricane"
+    },
+    {
+      "label": "Haiti Earthquake, Field Investigation on Comparative Performance of Alternative Humanitarian Logistic Structures",
+      "type": "Disaster",
+      "location": "Port-au-Prince and Leogane, Haiti",
+      "latlng": "19,-72",
+      "Month": "February",
+      "Year": "2010",
+      "purpose": "The purpose of this quick response study was to collect perishable data on the relief effort underway in Florida to assist earthquake stricken Haiti. Time was spent observing activities at warehouses, donation drives, Homestead Air Reserve Base, and planning meetings. The reception process of incoming evacuees from Haiti was also observed at Miami International Airport. Informal interviews were conducted with participants involved in these activities. Later, research was conducted in Port-au-Prince and Leogane, Haiti to attempt to understand the causes of the initial delay in the international relief as blamed on a shortage of trucks and warehouses, and the organization of this effort from the logistical viewpoint once started. The study consisted of 25 qualitative interviews with key informants from the United Nations agencies, the Red Cross, the representatives of grassroots organizations and the citizenry. ",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Haiti Earthquake, Assessing Community-scale Disruption and Restoration of Basic Needs in Post-earthquake Haiti",
+      "type": "Disaster",
+      "location": "Haiti",
+      "latlng": "19,-72",
+      "Month": "February",
+      "Year": "2010",
+      "purpose": " This was an initial field visit to gather perishable data following the January 2010 earthquake.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Haiti Earthquake, Assessing Community-scale Disruption and Restoration of Basic Needs in Post-earthquake Haiti",
+      "type": "Disaster",
+      "location": "Haiti",
+      "latlng": "19,-72",
+      "Month": "May",
+      "Year": "2010",
+      "purpose": " The visit was undertaken to collect perishable data from the January 2010 earthquake. Community meetings were held, interviews with NGOs and community leaders were conducted, and video data on building damage was collected.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Modeling the Interactions between Development and Regional Disaster Risk",
+      "type": "Disaster",
+      "location": "Padang, Indonesia",
+      "Month": "August",
+      "Year": "2010",
+      "purpose": "To collect information regarding the infrastructure in Padang and its development over time as well as details reflecting the impact of the 2009 earthquake on both infrastructure and the building processes. A goal was to collect both qualitative explanations of the trends in building construction as well as quantitative data on the infrastructure itself to use as input for a system dynamics model. ",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "San Bruno Gas Pipeline Explosion and Fire",
+      "type": "Disaster",
+      "location": "San Bruno, CA, U.S.A.",
+      "Month": "September",
+      "Year": "2010",
+      "purpose": "On September 9, 2010, in San Bruno, California, a suburb of San Francisco, a 30-inch steel natural gas pipeline exploded in flames, igniting a fire that ultimately killed seven residents and damaged or destroyed dozens of houses. In this RAPID project, we studied the San Bruno explosion and fire with the aims of (a) improving understanding of how urban fires spread and are suppressed, (b) supporting development and validation of next-generation urban fire simulation models, and (c) advancing theories of resilience. An interdisciplinary field team gathered data that provided a holistic account of the event across the engineering and social sciences. In particular, the team examined the composition and distribution of structures, rate and means of fire spread, local topography and weather conditions, and elements of multi-organizational coordination and decision making.",
+      "disaster-type": ["Natural","Human-Caused"],
+      "disaster-subtype": ["Fire", "Technological"]
+    },
+    {
+      "label": "Haiti Earthquake, Haiti Delaware Alliance Assessment Trip",
+      "type": "Disaster",
+      "location": "Jacmel, Haiti",
+      "Month": "October",
+      "Year": "2010",
+      "purpose": "HDA made a presentation to the Jacmel Town Council on their recovery project proposal in the wake of the Haiti earthquake. A DRC representative was invited to join as advisor to the group.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Tohoku Earthquake and Tsunami, Post-Earthquake Fires",
+      "type": "Disaster",
+      "location": "Tohoku, Japan",
+      "Month": "June",
+      "Year": "2011",
+      "Pupose": "The Tohoku earthquake and tsunami caused approximately 300 fires—more recorded fires than any other earthquake in history. In this RAPID project we studied the fire-related aspects of the March 2011 Japan earthquake with the aims to improve understanding of where, when, and how fires ignite, how fires spread through a neighborhood, and how they ignite and are suppressed in industrial facilities. The project involved three main steps: (1) collecting data on the fire-related aspects of the event through site visits, interviews with key informants, and secondary data sources; (2) compiling the data into easily usable, comprehensive databases that include all data on each fire and relevant auxiliary data in a consistent format; and (3) analyzing the data through descriptive statistics, fitting generalized linear statistical models to the ignition data, and comparing observations of spread to that estimated by a new physics-based urban fire spread model.",
+      "disaster-type": "Natural",
+      "disaster-subtype": ["Fire", "Earthquake", "Tsunami"]
+    },
+    {
+      "label": "Tohoku Earthquake and Tsunami, Volunteers and Nonprofit Organizations in Post-Kobe Japan",
+      "type": "Disaster",
+      "location": "Iwata Prefecture, Japan",
+      "Month": "June",
+      "Year": "2011",
+      "purpose": "To make initial contact and further define the scope of a planned DRC study anticipated for January 2012, which intends to study the engagement of volunteers and volunteer organizations following the Great East Japan earthquake of March 2011. Specifically, as an advance mission, the team was tasked with (1) identifying potential stakeholders to interview; (2) identifying sites to carry out these interviews; and (3) more narrowly defining potential research questions and issues.",
+      "disaster-type": "Natural",
+      "disaster-subtype": ["Earthquake", "Tsunami"]
+    },
+    {
+      "label": "Tohoku Earthquake and Tsunami, Volunteers and Nonprofit Organizations in Post-Kobe Japan",
+      "type": "Disaster",
+      "location": "Miyagi Prefecture, Japan",
+      "Month": "June",
+      "Year": "2011",
+      "purpose": "Participation on Earthquake Engineering Research Institute’s (EERI) social science reconnaissance team examining the impacts of the March 11 Great East Japan Earthquake.",
+      "disaster-type": "Natural",
+      "disaster-subtype": ["Earthquake", "Tsunami"]
+    },
+    {
+      "label": "VA Earthquake, Joint- Study",
+      "type": "Disaster",
+      "location": "Culpeper, VA; Mineral, VA; Louisa, VA; Washington, D.C., U.S.A.",
+      "Month": "August",
+      "Year": "2011",
+      "purpose": " DRC deployed a rapid response team with the objective of gathering data to (1) establish a comprehensive assessment of the impact of the earthquake, (2) understand actual versus perceived behavior during/after the event, and (3) identify lessons learned.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Modeling the Interactions between Development and Regional Disaster Risk",
+      "type": "Disaster",
+      "location": "Padang, Indonesia",
+      "Month": "December",
+      "Year": "2011",
+      "purpose": " To collect information regarding the infrastructure in Padang and its development over time as well as details reflecting the impact of the 2009 earthquake on both infrastructure and the building processes. A goal was to collect both qualitative explanations of the trends in building construction as well as quantitative data on the infrastructure itself to use as input for a system dynamics model.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Tornado Outbreak, National Weather Service Assessment",
+      "type": "Disaster",
+      "location": "Alabama; Tennessee, U.S.A.",
+      "Year": "2011",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Tornado"
+    },
+    {
+      "label": "Tohoku Earthquake, How Volunteer Organizations Function Following Catastrophes",
+      "type": "Disaster",
+      "location": "Tohuku, Japan",
+      "Month": "January",
+      "Year": "2011",
+      "purpose": " This project examined the response of various volunteer organizations to the Tohoku, Japan, earthquake, tsunami, and nuclear power plant failure. It looked specifically at how organizations cooperated with one another and how they integrated new members who came forward during the response. A special emphasis was given to those organizations addressing the special needs of persons with disabilities.",
+      "disaster-type": "Natural",
+      "disaster-subtype": ["Earthquake", "Tsunami"]
+    },
+    {
+      "label": "Northwest China Earthquake, Interagency Coordination and Response",
+      "type": "Disaster",
+      "location": "Minxian, Gansu Province,, China",
+      "Month": "May",
+      "Year": "2012",
+      "purpose": "Preliminary research into the impact of Superstorm Sandy on Staten Island and focused on the effect of the storm on hospitals. ",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Superstorm Sandy, NYC Hospital Response",
+      "type": "Disaster",
+      "location": "Staten Island, NY, U.S.A.",
+      "Month": "October",
+      "Year": "2012",
+      "purpose": "Preliminary research into the impact of Superstorm Sandy on Staten Island and focused on the effect of the storm on hospitals.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Hurricane"
+    },
+    {
+      "label": "Superstorm Sandy, NYC Emergent Groups Response",
+      "type": "Disaster",
+      "location": "Brooklyn and the Rockaways, NY, U.S.A.",
+      "Month": "November",
+      "Year": "2012",
+      "purpose": "Team members looked at how Occupy Sandy and other emergent groups organized relief efforts after Sandy.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Hurricane"
+    },
+    {
+      "label": "Moore, OK Tornado, Quick Response Reconnaissance Trip",
+      "type": "Disaster",
+      "location": "Moore, OK; Oklahoma City, OK, U.S.A.",
+      "Month": "May",
+      "Year": "2013",
+      "purpose": "This quick response research trip following the May 20th, 2013 tornado focused primarily on the mental health response effort.  It also covered other areas of interest, such as looting concerns, material convergence, and volunteer coordination.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Tornado"
+    },
+    {
+      "label": "Moore, OK Tornado, Collaborative Adaptive Sensing of the Atmosphere (CASA)",
+      "type": "Disaster",
+      "location": "Moore, OK; Oklahoma City, OK; New Castle, OK, U.S.A.",
+      "Month": "June",
+      "Year": "2013",
+      "purpose": "This field research studied public decision making during tornado warnings, including their perception and understanding of weather alerts in the Moore, Oklahoma City, and New Castle areas during the May 20th tornado. Researchers also observed the evacuation at the Will Rogers Airport first hand when the May 31st tornado struck the area. .",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Tornado"
+    },
+    {
+      "label": "Moore, OK Tornado, Collaborative CDI-Type II: Cyber Enabled Discovery System for Advanced Multidisciplinary Study of Humanitarian Logistics for Disaster Response",
+      "type": "Disaster",
+      "location": "Moore, OK; Oklahoma City, OK; Shawnee, Oklahoma, U.S.A.",
+      "Month": "June",
+      "Year": "2013",
+      "purpose": "Researchers deployed during this quick response field study looked at material convergence and motivations behind donations drives during the aftermath of the May 19th and 20th tornadoes.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Tornado"
+    },
+    {
+      "label": "Nepal Earthquake",
+      "type": "Disaster",
+      "location": "Kathmandu, Nepal",
+      "Month": "May",
+      "Year": "2015",
+      "purpose": "To examine the social vulnerabilities to and consequences of the April 25, 2015 earthquake in Nepal. The team took a broad view of the response, with special attention paid to nongovernmental organization coordination, the medical response to the earthquake, and infant feeding post-disaster.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Tropical Storm Erika, Florida Evacuation Decision-making",
+      "type": "Disaster",
+      "location": "Tallahassee, FL, U.S.A.",
+      "Month": "August",
+      "Year": "2015",
+      "purpose": " To collect data at the Emergency Operations Center in the state of Florida as a tropical system approached land. The tropical system (Erika) was projected at one point to turn into a hurricane. The storm dissipated before making landfall over the US but the decision-making process for evacuation was of interest for this trip because substantial rainfall had affected Florida immediately prior to the tropical system. DRC researchers sat in on major conference calls with the Hurricane Center, National Weather Service, and the governor of Florida..",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Haiti Earthquake, Field Investigation on Comparative Performance of Alternative Humanitarian Logistic Structures",
+      "type": "Disaster",
+      "location": "Miami, FL; Homestead, FL; Ft. Lauderdale, FL, U.S.A.",
+      "Month": "January",
+      "Year": "2010",
+      "purpose": "To collect perishable data on the relief effort underway in Florida to assist earthquake stricken Haiti. Time was spent observing activities at warehouses, donation drives, Homestead Air Reserve Base, and planning meetings. The reception process of incoming evacuees from Haiti was also observed at Miami International Airport. Informal interviews were conducted with participants involved in these activities. Later, research was conducted in Port-au-Prince and Leogane, Haiti to attempt to understand the causes of the initial delay in the international relief as blamed on a shortage of trucks and warehouses, and the organization of this effort from the logistical viewpoint once started. The study consisted of 25 qualitative interviews with key informants from the United Nations agencies, the Red Cross, the representatives of grassroots organizations and the citizenry.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    },
+    {
+      "label": "Nepal Earthquake, Infant Feeding, Maternal Vulnerability, and Well-being",
+      "type": "Disaster",
+      "location": "Kathmandu, Nepal",
+      "Month": "November",
+      "Year": "2015",
+      "purpose": " For two weeks in November 2015, Dr. DeYoung returned to Nepal for follow-up research on the Nepal earthwork recovery. Specifically, she collected data on the well-being of mothers living in temporary housing following the April 2015 earthquake. Her research questions focused on decision-making for infant feeding, psychological sense of community, and humanitarian complexities associated with the recent fuel/economic blockade.",
+      "disaster-type": "Natural",
+      "disaster-subtype": "Earthquake"
+    }
+  ]}
